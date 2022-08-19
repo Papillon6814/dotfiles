8986230eda5d8da698bf3d03bfc04b1396dca0a6
Papillon6814 <so1rou.k@gmail.com>
2022-08-19 16:09:07 +0900

    2022年 8月19日 金曜日 16時09分07秒 JST

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -50,13 +50,7 @@ packer.startup(function(use)
   use {
     "folke/todo-comments.nvim",
     requires = "nvim-lua/plenary.nvim",
-    config = function()
-      require("todo-comments").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
+    config = function() require("todo-comments").setup{} end
   }
 
   -- Settings for each language
