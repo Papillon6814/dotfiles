4f30d9cc02c3ad7a762649a96d44ead36f0b54d6
Papillon6814 <so1rou.k@gmail.com>
2023-06-25 19:22:42 +0900

    Sun Jun 25 19:22:42 JST 2023

--- nvim/lua/papillon6814/plugins.lua
+++ nvim/lua/papillon6814/plugins.lua
@@ -1,112 +1,112 @@
 local status, packer = pcall(require, "packer")
 if not status then
-	print("Packer is not installed")
-	return
+  print("Packer is not installed")
+  return
 end
 
 vim.cmd([[packadd packer.nvim]])
 
 packer.startup(function(use)
-	use("wbthomason/packer.nvim")
+  use("wbthomason/packer.nvim")
 
-	-- Color Theme
-	use("tanvirtin/monokai.nvim")
-	use("sainnhe/sonokai")
+  -- Color Theme
+  use("tanvirtin/monokai.nvim")
+  use("sainnhe/sonokai")
 
-	-- Indent Indicator
-	use("lukas-reineke/indent-blankline.nvim")
+  -- Indent Indicator
+  use("lukas-reineke/indent-blankline.nvim")
 
-	-- Status line decorator
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons", opt = true },
-	})
-	-- LSP config
-	use("neovim/nvim-lspconfig")
+  -- Status line decorator
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons", opt = true },
+  })
+  -- LSP config
+  use("neovim/nvim-lspconfig")
 
-	-- LSP package manager
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
+  -- LSP package manager
+  use("williamboman/mason.nvim")
+  use("williamboman/mason-lspconfig.nvim")
 
-	-- Formatter
-	use({ "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" })
+  -- Formatter
+  use({ "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" })
 
-	-- Highlight Parser
-	use({
-		"nvim-treesitter/nvim-treesitter",
-		run = ":TSUpdate",
-	})
+  -- Highlight Parser
+  use({
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  })
 
-	-- Autotag
-	use("windwp/nvim-ts-autotag")
+  -- Autotag
+  use("windwp/nvim-ts-autotag")
 
-	-- Auto pair brackets
-	use("windwp/nvim-autopairs")
+  -- Auto pair brackets
+  use("windwp/nvim-autopairs")
 
-	-- Iconset for Fuzzy Finder
-	use("kyazdani42/nvim-web-devicons")
+  -- Iconset for Fuzzy Finder
+  use("kyazdani42/nvim-web-devicons")
 
-	-- Fuzzy Finder
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
-	-- Fuzzy Finder as a file browser
-	use({ "nvim-telescope/telescope-file-browser.nvim" })
+  -- Fuzzy Finder
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
+  -- Fuzzy Finder as a file browser
+  use({ "nvim-telescope/telescope-file-browser.nvim" })
 
-	-- Prettier
-	use("MunifTanjim/prettier.nvim")
-	use("prettier/vim-prettier")
+  -- Prettier
+  use("MunifTanjim/prettier.nvim")
+  use("prettier/vim-prettier")
 
-	-- Git Change indicator
-	use("lewis6991/gitsigns.nvim")
+  -- Git Change indicator
+  use("lewis6991/gitsigns.nvim")
 
-	-- Git commands in neovim
-	use("dinhhuy258/git.nvim")
+  -- Git commands in neovim
+  use("tpope/vim-fugitive")
 
-	-- Annotaion comments
-	use({
-		"folke/todo-comments.nvim",
-		requires = "nvim-lua/plenary.nvim",
-		config = function()
-			require("todo-comments").setup({})
-		end,
-	})
+  -- Annotaion comments
+  use({
+    "folke/todo-comments.nvim",
+    requires = "nvim-lua/plenary.nvim",
+    config = function()
+      require("todo-comments").setup({})
+    end,
+  })
 
-	-- Search a first unique character in line
-	use("unblevable/quick-scope")
+  -- Search a first unique character in line
+  use("unblevable/quick-scope")
 
-	-- Visible color
-	use("norcalli/nvim-colorizer.lua")
+  -- Visible color
+  use("norcalli/nvim-colorizer.lua")
 
-	-- Auto close brackets
-	use("rstacruz/vim-closer")
+  -- Auto close brackets
+  use("rstacruz/vim-closer")
 
-	-- GoToDefinition
-	use({
-		"glepnir/lspsaga.nvim",
-		branch = "main",
-		config = function()
-			require("lspsaga").setup({})
-		end,
-		requires = { { "nvim-tree/nvim-web-devicons" } },
-	})
+  -- GoToDefinition
+  use({
+    "glepnir/lspsaga.nvim",
+    branch = "main",
+    config = function()
+      require("lspsaga").setup({})
+    end,
+    requires = { { "nvim-tree/nvim-web-devicons" } },
+  })
 
-	-- Snippet
-	use("honza/vim-snippets")
-	use("SirVer/ultisnips")
+  -- Snippet
+  use("honza/vim-snippets")
+  use("SirVer/ultisnips")
 
-	-- Highlight same spell words
-	use("RRethy/vim-illuminate")
+  -- Highlight same spell words
+  use("RRethy/vim-illuminate")
 
-	-- ==========================================================
-	-- Settings for each language
-	-- ==========================================================
+  -- ==========================================================
+  -- Settings for each language
+  -- ==========================================================
 
-	-- Elixir
-	use("elixir-editors/vim-elixir")
-	use({ "mhanberg/elixir.nvim", requires = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" } })
+  -- Elixir
+  use("elixir-editors/vim-elixir")
+  use({ "mhanberg/elixir.nvim", requires = { "neovim/nvim-lspconfig", "nvim-lua/plenary.nvim" } })
 
-	-- Go
-	use("johejo/gomod.vim")
+  -- Go
+  use("johejo/gomod.vim")
 end)
