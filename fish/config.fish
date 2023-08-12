97d3fc251478cb6ca5ceba5c0a5d37d4b22e0ecc
Papillon6814 <so1rou.k@gmail.com>
2023-08-12 13:42:30 +0900

    Sat Aug 12 13:42:30 JST 2023

--- fish/config.fish
+++ fish/config.fish
@@ -85,3 +85,24 @@ export PATH="$PATH:/Users/kuno-soichiro/.foundry/bin"
 export STARSHIP_CONFIG=~/.starship.toml
 
 #starship init fish | source
+
+# Attach tmux session if it does not exist
+function attach_tmux_session_if_needed
+    set ID (tmux list-sessions)
+    if test -z "$ID"
+        tmux new-session
+        return
+    end
+
+    set new_session "Create New Session" 
+    set ID (echo $ID\n$new_session | peco --on-cancel=error | cut -d: -f1)
+    if test "$ID" = "$new_session"
+        tmux new-session
+    else if test -n "$ID"
+        tmux attach-session -t "$ID"
+    end
+end
+
+if test -z $TMUX && status --is-login
+    attach_tmux_session_if_needed
+end
