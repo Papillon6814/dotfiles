c077d518233b16085e90e8196fea883df4d0d3a7
Papillon <papillon@Papillons-MBP.lan>
2023-12-22 01:26:50 +0900

    refactor

--- fish/config.fish
+++ fish/config.fish
@@ -4,34 +4,14 @@ set -x PYENV_ROOT $HOME/.pyenv
 set -x PATH $PYENV_ROOT/shims $PATH
 set -x PATH $HOME/.local/bin:$PATH
 
-eval (anyenv init - | source)
 source (pyenv init - | psub)
 
 set -x PATH /Users/papillon/Library/Android/sdk/platform-tools $PATH
 set -x PATH /Users/kuno-soichiro/.rover/bin $PATH
 set -x JAVA_HOME /opt/homebrew/Cellar/openjdk/16.0.1/libexec/openjdk.jdk/Contents/Home
-set PATH /opt/homebrew/opt/php@7.4/bin $PATH
-
-# e-server
-set -x CLOUD_SQL_HOST /tmp/cloudsql/e-server-339914:asia-northeast1:e-server-database/.s.PGSQL.5432
-set -x CLOUD_SQL_PASSWORD EhO9KMjIu?FJuhaP
-#set -x DATABASE_URL postgres://postgres:EhO9KMjIu%3FFJuhaP@34.85.0.85:5432/e_server_prod
-set -x SECRET_KEY_BASE gfjEWDcEOLGjZr8UzMIWb6iu4En+O6h81CMSOHqzIdIss2o5gM4yz3+OJp0wB0uY
-
 export PATH="$HOME/.cargo/bin:$PATH"
 
-set -gx LDFLAGS "-L/opt/homebrew/opt/php@8.0/lib"
-set -gx CPPFLAGS "-I/opt/homebrew/opt/php@8.0/include"
-
 set fish_greeting Blacks are humble, sit down.
-
-#set -x CC clang
-set -x KERL_BUILD_DOCS no
-set -x KERL_CONFIGURE_OPTIONS --with-ssl=(brew --prefix openssl)
-
-#set -x DATABASE_URL localhost://postgres:postgres@localhost:5432/milka
-#set -x SECRET_KEY_BASE KL6jdYa8VzDIg1TjqQcGZsaGSnd43grQYWrl95dYjn/SJB7KABj7N4BWgaCxSF81
-set -x RUST_BACKTRACE 1
 
 # alias
 # alias cdg='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs/Documents/Github/'
@@ -49,43 +29,13 @@ alias cdc='cd ~/Documents/Github/e-players-webapp'
 alias cds='cd ~/Documents/Github/e-server'
 alias cdz='cd ~/Documents/Github/zenn'
 
-# nodebrew
-# set -x PATH $PATH $HOME/.nodebrew/current/bin
-
-# tkinter
-set -x PATH "/usr/local/opt/tcl-tk/bin" $PATH
-set -x LDFLAGS "-L/usr/local/opt/tcl-tk/lib"
-set -x CPPFLAGS "-I/usr/local/opt/tcl-tk/include"
-set -x PKG_CONFIG_PATH "/usr/local/opt/tcl-tk/lib/pkgconfig"
-set -x PYTHON_CONFIGURE_OPTS "--with-tcltk-includes='-I/usr/local/opt/tcl-tk/include' --with-tcltk-libs='-L/usr/local/opt/tcl-tk/lib -ltcl8.6 -ltk8.6'"
-
-# Golang
-set -x GOBIN /opt/homebrew/bin
-set -x PATH $GOBIN $PATH
-#eval "$(goenv init -)"
-set -x GOROOT /opt/homebrew/opt/go@1.20/libexec
-
-#set -x GOROOT /opt/homebrew/Cellar/go/1.20.3/libexec
-#set -x GOPATH /opt/homebrew/Cellar/go/1.20.3/
-# source ~/.asdf/asdf.fish
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
-
-alias medis '/bin/bash ~/papillon-bin/medis'
 set -g fish_user_paths "/usr/local/opt/llvm/bin" $fish_user_paths
 
-#source /usr/local/bin/asdf/asdf.fish
 source /opt/homebrew/opt/asdf/libexec/asdf.fish
 fish_add_path /opt/homebrew/opt/openjdk/bin
 
-
 # The next line updates PATH for the Google Cloud SDK.
 if [ -f '/Users/papillon/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/papillon/Downloads/google-cloud-sdk/path.fish.inc'; end
-
-export PATH="$PATH:/Users/kuno-soichiro/.foundry/bin"
-export STARSHIP_CONFIG=~/.starship.toml
-
-#starship init fish | source
 
 # Attach tmux session if it does not exist
 function attach_tmux_session_if_needed
@@ -107,11 +57,4 @@ end
 if test -z $TMUX && status --is-login
     attach_tmux_session_if_needed
 end
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /Users/kuno-souichiro/.anyenv/envs/pyenv/versions/anaconda3-2023.07-2/bin/conda
-    eval /Users/kuno-souichiro/.anyenv/envs/pyenv/versions/anaconda3-2023.07-2/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
+status --is-interactive; and ~/.rbenv/bin/rbenv init - fish | source
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/kuno-soichiro/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/Users/kuno-soichiro/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/Users/kuno-soichiro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/Users/kuno-soichiro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/kuno-soichiro/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
+local package_path_str = "/Users/papillon/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?.lua;/Users/papillon/.cache/nvim/packer_hererocks/2.1.1700008891/share/lua/5.1/?/init.lua;/Users/papillon/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?.lua;/Users/papillon/.cache/nvim/packer_hererocks/2.1.1700008891/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/papillon/.cache/nvim/packer_hererocks/2.1.1700008891/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,172 +76,173 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   ["elixir.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/elixir.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/elixir.nvim",
     url = "https://github.com/mhanberg/elixir.nvim"
   },
   ["gitsigns.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/gitsigns.nvim",
     url = "https://github.com/lewis6991/gitsigns.nvim"
   },
   ["gomod.vim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/gomod.vim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/gomod.vim",
     url = "https://github.com/johejo/gomod.vim"
   },
   ["indent-blankline.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/indent-blankline.nvim",
     url = "https://github.com/lukas-reineke/indent-blankline.nvim"
   },
   ["lspsaga.nvim"] = {
     config = { "\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\flspsaga\frequire\0" },
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/lspsaga.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/lspsaga.nvim",
     url = "https://github.com/glepnir/lspsaga.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["monokai.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/monokai.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/monokai.nvim",
     url = "https://github.com/tanvirtin/monokai.nvim"
   },
   ["null-ls.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
     url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
   },
   ["nvim-autopairs"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-colorizer.lua"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/nvim-colorizer.lua",
     url = "https://github.com/norcalli/nvim-colorizer.lua"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-ts-autotag"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/nvim-ts-autotag",
     url = "https://github.com/windwp/nvim-ts-autotag"
   },
   ["nvim-web-devicons"] = {
-    loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/nvim-web-devicons",
+    loaded = false,
+    needs_bufread = false,
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["prettier.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/prettier.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/prettier.nvim",
     url = "https://github.com/MunifTanjim/prettier.nvim"
   },
   ["quick-scope"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/quick-scope",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/quick-scope",
     url = "https://github.com/unblevable/quick-scope"
   },
   sonokai = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/sonokai",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/sonokai",
     url = "https://github.com/sainnhe/sonokai"
   },
   ["telescope-file-browser.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/telescope-file-browser.nvim",
     url = "https://github.com/nvim-telescope/telescope-file-browser.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["todo-comments.nvim"] = {
     config = { "\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18todo-comments\frequire\0" },
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/todo-comments.nvim",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/todo-comments.nvim",
     url = "https://github.com/folke/todo-comments.nvim"
   },
   ultisnips = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/ultisnips",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/ultisnips",
     url = "https://github.com/SirVer/ultisnips"
   },
   ["vim-closer"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/vim-closer",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/vim-closer",
     url = "https://github.com/rstacruz/vim-closer"
   },
   ["vim-elixir"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/vim-elixir",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/vim-elixir",
     url = "https://github.com/elixir-editors/vim-elixir"
   },
   ["vim-fugitive"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/vim-fugitive",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/vim-fugitive",
     url = "https://github.com/tpope/vim-fugitive"
   },
   ["vim-illuminate"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/vim-illuminate",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/vim-illuminate",
     url = "https://github.com/RRethy/vim-illuminate"
   },
   ["vim-prettier"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/vim-prettier",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/vim-prettier",
     url = "https://github.com/prettier/vim-prettier"
   },
   ["vim-snippets"] = {
     loaded = true,
-    path = "/Users/kuno-soichiro/.local/share/nvim/site/pack/packer/start/vim-snippets",
+    path = "/Users/papillon/.local/share/nvim/site/pack/packer/start/vim-snippets",
     url = "https://github.com/honza/vim-snippets"
   }
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: lspsaga.nvim
-time([[Config for lspsaga.nvim]], true)
-try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\flspsaga\frequire\0", "config", "lspsaga.nvim")
-time([[Config for lspsaga.nvim]], false)
 -- Config for: todo-comments.nvim
 time([[Config for todo-comments.nvim]], true)
 try_loadstring("\27LJ\2\n?\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\18todo-comments\frequire\0", "config", "todo-comments.nvim")
 time([[Config for todo-comments.nvim]], false)
+-- Config for: lspsaga.nvim
+time([[Config for lspsaga.nvim]], true)
+try_loadstring("\27LJ\2\n9\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\flspsaga\frequire\0", "config", "lspsaga.nvim")
+time([[Config for lspsaga.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
